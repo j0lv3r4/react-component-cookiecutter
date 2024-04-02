232fa01858d245e0bf2b91ed4fb64cbaaa88c9a5
Juan Olvera <juan@jolvera.dev>
2024-04-02 13:11:19 -0500

    fix: import

--- {{ cookiecutter.project_name_plural }}/index.ts
+++ {{ cookiecutter.project_name_plural }}/index.ts
@@ -2,7 +2,7 @@ import { {{ cookiecutter.project_name_plural }}Page as Page } from "./{{ cookiec
 import { {{ cookiecutter.project_name_plural }}Form as Form } from "./{{ cookiecutter.project_name_plural }}.form.tsx";
 import { {{ cookiecutter.project_name_plural }}List as List } from "./{{ cookiecutter.project_name_plural }}.list.tsx";
 import * as T from "./{{ cookiecutter.project_name_plural }}.types.ts";
-import * as C from "./NounInPlural.const.ts";
+import * as C from "./{{ cookiecutter.project_name_plural }}.const.ts";
 import * as H from "./{{ cookiecutter.project_name_plural }}.hooks.ts";
 
 export { Page, Form, List, T, C, H };
